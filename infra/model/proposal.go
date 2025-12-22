764bbbd0569cadb2c26222774a59b90cf563deac
Eagle233 <eagle233real@outlook.com>
2025-12-22 10:14:52 +0800

    ci: 新增自动生成Swagger

--- infra/model/proposal.go
+++ infra/model/proposal.go
@@ -1,3 +1,17 @@
+// Copyright 2025 Boyuan-IT-Club
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package model
 
 import (
@@ -5,13 +19,14 @@ import (
 )
 
 type Proposal struct {
-	ID        string    `bson:"_id,omitempty"      json:"id"`
-	UserID    string    `bson:"userId"             json:"userId"`  // 提出Proposal的用户ID
-	Title     string    `bson:"title"              json:"title"`   // 标题
-	Content   string    `bson:"content"            json:"content"` // 描述的内容
-	Deleted   bool      `bson:"deleted" json:"deleted"`            // 删除标记
-	Status    int32     `bson:"status"             json:"status"`  // 提案的状态，0: 待审核，1: 通过，2: 拒绝
-	Course    *Course   `bson:"course"             json:"course"`  // 课程信息，包含教师的ID（未创建不需要ID）
-	CreatedAt time.Time `bson:"createdAt"          json:"createdAt"`
-	UpdatedAt time.Time `bson:"updatedAt"          json:"updatedAt"`
+	ID         string    `bson:"_id,omitempty"      json:"id"`
+	UserID     string    `bson:"userId"             json:"userId"`     // 提出Proposal的用户ID
+	Title      string    `bson:"title"              json:"title"`      // 标题
+	Content    string    `bson:"content"            json:"content"`    // 描述的内容
+	Deleted    bool      `bson:"deleted" json:"deleted"`               // 删除标记
+	Status     int32     `bson:"status"             json:"status"`     // 提案的状态，0: 待审核，1: 通过，2: 拒绝
+	Course     *Course   `bson:"course"             json:"course"`     // 课程信息，包含教师的ID（未创建不需要ID）
+	AgreeCount int64     `bson:"agreeCount"         json:"agreeCount"` // 同意数
+	CreatedAt  time.Time `bson:"createdAt"          json:"createdAt"`
+	UpdatedAt  time.Time `bson:"updatedAt"          json:"updatedAt"`
 }
