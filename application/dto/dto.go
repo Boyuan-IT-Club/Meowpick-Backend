14ebcaaefd6c1673a19f2c1edc4bfffb1fcce753
Eagle233 <eagle233real@outlook.com>
2026-04-11 00:25:43 +0800

    fix(page): 从1开始

--- application/dto/dto.go
+++ application/dto/dto.go
@@ -36,8 +36,8 @@ type IPageParam interface {
 }
 
 func (p *PageParam) UnWrap() (int64, int64) {
-	if p.Page < 0 {
-		p.Page = 0
+	if p.Page < 1 {
+		p.Page = 1
 	}
 	if p.PageSize <= 0 || p.PageSize > 100 {
 		p.PageSize = 10
